4f42b77d5bf3668fb7c7c1a7f0cb6a24cd1ae962
Frank Guchelaar <fguchelaar@gmail.com>
2016-12-20 19:35:24 +0100

    nice and fast solution. didn't take a lot of time. Happy coder :)

---
+++ AdventOfCode2016/aoc-day20/main.swift
@@ -0,0 +1,57 @@
+//
+//  main.swift
+//  aoc-day20
+//
+//  Created by Frank Guchelaar on 20/12/2016.
+//  Copyright © 2016 Awesomation. All rights reserved.
+//
+
+import Foundation
+
+var input = try String(contentsOfFile: "day20/input.txt").trimmingCharacters(in: .whitespacesAndNewlines)
+
+var ranges = input
+    .components(separatedBy: .newlines)
+    .map { (line) -> (low: Int, high: Int) in
+        let parts = line.components(separatedBy: "-")
+        return (Int(parts[0])!, Int(parts[1])!)
+    }
+    .sorted { $0.low < $1.low }
+
+measure {
+    
+    // Part One
+    
+    var low = 0
+    for range in ranges {
+        
+        if range.low > low+1 {
+            print ("1) \(low + 1)");
+            break
+        }
+        else {
+            low = max(low, range.high)
+        }
+    }
+}
+
+measure {
+    var low = 0
+    var high = 4294967295
+    
+    print ("Total of ranges: \(ranges.count)")
+    
+    var sum = 0
+    for (idx, range) in ranges.enumerated() {
+
+        if range.low > low+1 {
+            sum += range.low - (low+1)
+        }
+        low = max(low, range.high)
+    }
+    // add difference between high and high of highest range
+    let highest = ranges.sorted { $0.high > $1.high}.first!
+    sum += high - highest.high
+    
+    print ("2) \(sum)")
+}
